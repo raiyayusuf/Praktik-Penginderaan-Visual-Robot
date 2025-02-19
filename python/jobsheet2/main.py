9f18ebf422ed26353fb01bea2a630f3ecca2e864
Raiyayusuf11 <raiyayusuf123@gmail.com>
2025-02-19 12:43:07 +0700

    anyar

--- python/jobsheet2/main.py
+++ python/jobsheet2/main.py
@@ -3,8 +3,15 @@ print('*         *')
 print('*  O   O  *')
 print('*         *')
 print('*         *')
-print('*         *')
 print('***********')
 print('  ||   ||  ')
 print('  ||   ||  ')
 print('  ==   ==  ')
+
+
+a = 8
+b = c = 5
+
+hasil = a + b 
+
+print(hasil)
