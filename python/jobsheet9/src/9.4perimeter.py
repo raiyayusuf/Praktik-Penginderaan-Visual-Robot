b72964be3145f465b4dd2f5cbfff9ba985fcab44
Raiyayusuf11 <raiyayusuf123@gmail.com>
2025-05-14 14:03:48 +0700

    upLS9

--- python/jobsheet9/src/9.4perimeter.py
+++ python/jobsheet9/src/9.4perimeter.py
@@ -1,6 +1,6 @@
 import cv2
 
-path_gambar = r'python\jobsheet9\img\guppy.jpeg'
+path_gambar = r'python\jobsheet9\img\guppyBiru.png'
 citra = cv2.imread(path_gambar, 0)
 
 kontur, hierarki = cv2.findContours(citra, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
